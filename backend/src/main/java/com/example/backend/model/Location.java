f2335d1ad73bbb28b54c67f05a6cfe9a6437eeb7
emea94 <emea94@web.de>
2024-03-25 18:43:42 +0100

    Add lombok annotation to find correct collection in database

--- backend/src/main/java/com/example/backend/model/Location.java
+++ backend/src/main/java/com/example/backend/model/Location.java
@@ -1,7 +1,9 @@
 package com.example.backend.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document("locations")
 public record Location(
         @Id
         String LocationId,
