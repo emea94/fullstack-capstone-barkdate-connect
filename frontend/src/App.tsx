920935b5ed97c38379203d7ed1e9e0f11d3ace0f
emea94 <emea94@web.de>
2024-03-25 18:17:10 +0100

    Create method fetchLocations, useEffect, useState in Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,44 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Link, Route, Routes} from "react-router-dom";
+import LocationGallery from "./components/LocationGallery.tsx";
+
 export default function App() {
+  const [location, setLocation] = useState<Location[]>([])
+
+    useEffect(() => {
+        fetchLocation();
+    }, []);
+
+  const fetchLocation = () => {
+    axios.get('/api/bark-dates')
+        .then(response => {
+          setLocation(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching Locations: ', error);
+        });
+  }
 
   return (
-        <h1>App</h1>
+      <>
+          <header>
+              <div className={"HeaderComponent"}>
+                  <div className={"HeaderWrapper"}>
+                      <Link to={"/"} className={"HeaderLink"}>
+                          <h1 className={"HeaderTitle"}>Bark Dates</h1>
+                      </Link>
+                  </div>
+              </div>
+          </header>
+          <Routes>
+              <Route path={"/"} element={
+                  location.length > 0
+                      ?
+                      <LocationGallery locations={location} fetchLocations={fetchLocation}/>
+                      :
+                      <h1 className={"notfoundtag"}>No locations found</h1>}/>
+          </Routes>
+      </>
   )
 }
