e0dacfa3cb1d1eaa72364ba828b69486b587b73b
emea94 <emea94@web.de>
2024-03-25 16:15:39 +0100

    delete unused interfaces

--- backend/src/main/java/com/example/backend/repository/DateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.Date;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DateRepository extends MongoRepository<Date, String> {
-}
