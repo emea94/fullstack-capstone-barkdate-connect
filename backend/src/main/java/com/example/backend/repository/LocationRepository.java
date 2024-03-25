b50ab625fc40d051dca20b8d28f5a75e83694b56
emea94 <emea94@web.de>
2024-03-25 16:14:36 +0100

    delete unused import statement

--- backend/src/main/java/com/example/backend/repository/LocationRepository.java
+++ backend/src/main/java/com/example/backend/repository/LocationRepository.java
@@ -1,6 +1,5 @@
 package com.example.backend.repository;
 
-import com.example.backend.model.Date;
 import com.example.backend.model.Location;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
