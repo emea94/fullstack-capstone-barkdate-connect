4846c240b97a4ac838b0db76757dc2c5e9a3474d
emea94 <emea94@web.de>
2024-03-27 10:33:19 +0100

    add some more changes for Sonarcloud issues

--- frontend/src/components/LocationCard.tsx
+++ frontend/src/components/LocationCard.tsx
@@ -20,7 +20,7 @@ export default function LocationCard(props: Readonly<LocationCardProps>) {
             className={"LocationCard"}
             onClick={navigateToDetail}
             onKeyDown={navigateToDetail}
-            tabIndex={0}
+            role="button"
             aria-label={`Navigate to ${props.location.city}`}
         >
             <div className={"LocationCity"}>{props.location.city}</div>
