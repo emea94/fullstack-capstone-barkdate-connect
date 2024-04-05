9b34661e20a6a4c5b266fa035001afae0c586130
emea94 <emea94@web.de>
2024-04-05 09:56:09 +0200

    add css stylesheet to main.tsx add bootstrap components for App add styling in index.cxx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,49 +5,49 @@ import LocationGallery from "./components/LocationGallery.tsx";
 import {Location} from './types/Location.ts'
 import NewLocation from "./components/NewLocation.tsx";
 import Logo from "./assets/BarkDateLogo.png"
+import {Nav, Navbar} from "react-bootstrap";
 
 
 export default function App() {
-  const [location, setLocation] = useState<Location[]>([])
+    const [location, setLocation] = useState<Location[]>([])
 
     useEffect(() => {
         fetchLocation();
     }, []);
 
-  const fetchLocation = () => {
-    axios.get('/api/bark-dates')
-        .then(response => {
-          setLocation(response.data);
-        })
-        .catch(error => {
-          console.error('Error fetching Locations: ', error);
-        });
-  }
+    const fetchLocation = () => {
+        axios.get('/api/bark-dates')
+            .then(response => {
+                setLocation(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching Locations: ', error);
+            });
+    }
 
-  return (
-      <>
-          <header>
-              <div className={"HeaderComponent"}>
-                  <div className={"HeaderWrapper"}>
-                      <Link to={"/"} className={"HeaderLink"}>
-                          <img className={"HeaderLogo"} src={Logo} alt={"Barkdate Logo"}/>
-                      </Link>
-                  </div>
-                  <div className={"HeaderNavigation"}>
-                      <Link to={"/new-location"} className={"HeaderNavLink"}>Neuen Standort hinzufügen
-                      </Link>
-                  </div>
-              </div>
-          </header>
-          <Routes>
-              <Route path={"/"} element={
-                  location.length > 0
-                      ?
-                      <LocationGallery locations={location} fetchLocations={fetchLocation}/>
-                      :
-                      <h1 className={"notfoundtag"}>No locations found</h1>}/>
-              <Route path={"/new-location"} element={<NewLocation saveNewLocation={setLocation}/>}/>
-          </Routes>
-      </>
-  )
+    return (
+        <>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand>
+                    <Link to={"/"} className={"HeaderLink"}>
+                        <img className={"HeaderLogo"} src={Logo} alt={"Barkdate Logo"} />
+                    </Link>
+                </Navbar.Brand>
+                <Nav className="ml-auto">
+                    <Link to={"/"} className={"HeaderNavLink"}>Home</Link>
+                    <Link to={"/new-location"} className={"HeaderNavLink"}>Standort hinzufügen</Link>
+                </Nav>
+            </Navbar>
+
+            <Routes>
+                <Route path={"/"} element={
+                    location.length > 0
+                        ?
+                        <LocationGallery locations={location} fetchLocations={fetchLocation}/>
+                        :
+                        <h1 className={"notfoundtag"}>No locations found</h1>}/>
+                <Route path={"/new-location"} element={<NewLocation saveNewLocation={setLocation}/>}/>
+            </Routes>
+        </>
+    )
 }
